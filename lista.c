2e95c41bb414e5fb94b2880742cbce6dd3c0e9bd
Micael-Martins <micael.mdemoura@gmail.com>
2023-10-19 10:45:25 -0300

    teste muito triste

---
+++ lista.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// primeiramente, fazer uma area que aloque cada scanf em uma variavél
+
+int main(){
+    int a,b,c,d,e,f,g,h,i,j;
+
+    scanf("%d %d %d %d %d %d %d %d %d %d", &a, &b, &c, &d, &e, &f, &g, &h, &i, &j);
+
+    return 0;
+}
